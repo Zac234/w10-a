8817f41a7dcfff167169a092ead3f147fb689ac3
Zach <zacbenincasa@gamil.com>
2022-07-12 16:29:05 -0600

    added all steps from 1 to 11 to the best of my ability

---
+++ W/js/app.js
@@ -0,0 +1,40 @@
+let my_city = {
+    name:"Tiny Town",
+    population: 13907,
+    is_capital: false,
+    communities:{
+        name:"Summervale",
+        population:130,
+
+        name:"Funktown",
+        population:97,
+
+        name:"Quasar5",
+        population:83,
+
+        name:"Briton",
+        population:100,
+    
+
+        name:"Bosche",
+        population:125
+    }
+    
+
+    
+        
+    
+}
+
+if (is_capital=true){
+    console.log([name]&&[population])
+}else {
+    console.log("not important enough")
+}
+
+
+let counter=0;
+while (counter<5&&{my_city}[communities][population<100]){
+    console.log(communities)
+    counter=counter+1
+}
